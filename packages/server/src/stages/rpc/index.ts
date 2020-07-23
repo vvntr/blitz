vvntr/blitz

commit e1825ccf384f9b717d09043e94700a9a921a5f80

Extend API Handler Template to allow custom connection methods Not every database client has a `.connect` method, but most require some sort of setup. This commit allows people to override `.connect` with their own connection routine by exporting a `connect` method from `db/index.ts`.

packages/server/src/stages/rpc/index.ts
@@ -79,13 +79,15 @@ import resolverModule from '${originalPath}'
 import {getAllMiddlewareForModule} from '@blitzjs/core'
 import {rpcApiHandler} from '@blitzjs/server'
 let db
+let connect
 try {
   db = require('db').default
+  connect = require('db').connect ?? db.connect
 }catch(err){}
 export default rpcApiHandler(
   resolverModule,
   getAllMiddlewareForModule(resolverModule),
-  () => db && db.connect(),
+  () => db && connect?.(),
 )
 export const config = {
   api: {

commit 20ec4ae998713536c6eaf83675480349e5f3b510

Fix crash introduced by e1825cc
Reported by @flybayer: https://blitzjs.slack.com/archives/CUM2C68T1/p1595511074205800
Running `connect = db.connect` breaks `this` usage for `db.connect`, which is the reason it broke.

packages/server/src/stages/rpc/index.ts
@@ -90,12 +90,12 @@ let db
 let connect
 try {
   db = require('db').default
-  connect = require('db').connect ?? db.connect
+  connect = require('db').connect ?? () => db.connect()
 }catch(err){}
 export default rpcApiHandler(
   resolverModule,
   getAllMiddlewareForModule(resolverModule),
-  () => db && connect?.(),
+  () => db && connect(),
 )
 export const config = {
   api: {